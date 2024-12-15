3f8ea39d498d235d95434eda224fb365e53f9d2f
Aadhar Kumar <159239724+aad-neenopal@users.noreply.github.com>
2024-12-15 17:43:46 +0530

    [FIX] fixed notification urls

--- notifications/urls.py
+++ notifications/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notifications', views.notifications),
-    path('notifications/counts', views.notifications_counts),
-    path('notifications/<int:notification_id>', views.particular_notification),
+    path('notifications', views.notifications, name="notifications"),
+    path('notifications/<int:notification_id>/mark', views.mark_notification, name="mark-notification"),
+    path('notifications/<int:notification_id>/delete', views.delete_notification, name="delete-notification"),
 ]
